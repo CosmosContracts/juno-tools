1628f66cf388191ca31446cb90515d7126920622
Arda Nakışçı <anakisci@gmail.com>
2022-01-11 13:47:28 +0300

    Add keplr and wallet service

---
+++ services/keplr.ts
@@ -0,0 +1,68 @@
+import { useEffect, useState } from 'react'
+import { OfflineSigner } from '@cosmjs/proto-signing'
+import { SigningCosmWasmClient } from '@cosmjs/cosmwasm-stargate'
+import { getConfig, keplrConfig, AppConfig } from '../config'
+import { useWallet } from './wallet'
+
+export async function createClient(
+  signer: OfflineSigner,
+  network: string
+): Promise<SigningCosmWasmClient> {
+  return SigningCosmWasmClient.connectWithSigner(
+    getConfig(network).rpcUrl,
+    signer
+  )
+}
+
+export async function loadKeplrWallet(
+  config: AppConfig
+): Promise<OfflineSigner> {
+  const anyWindow: any = window
+  if (!anyWindow.getOfflineSigner) {
+    throw new Error('Keplr extension is not available')
+  }
+
+  await anyWindow.keplr.enable(config.chainId)
+  await anyWindow.keplr.experimentalSuggestChain(keplrConfig(config))
+
+  const signer = await anyWindow.getOfflineSignerAuto(config.chainId)
+  signer.signAmino = signer.signAmino ?? signer.sign
+
+  return Promise.resolve(signer)
+}
+
+export function useKeplr() {
+  const { clear, init, initialized, network } = useWallet()
+  const [initializing, setInitializing] = useState(false)
+  const config = getConfig(network)
+
+  const disconnect = () => {
+    localStorage.clear()
+    clear()
+  }
+
+  const connect = () => {
+    setInitializing(true)
+
+    loadKeplrWallet(config)
+      .then((signer) => {
+        init(signer)
+      })
+      .catch((err) => {
+        setInitializing(false)
+        // errorToast(err.message)
+      })
+  }
+
+  useEffect(() => {
+    if (!initialized) return
+
+    setInitializing(false)
+  }, [initialized])
+
+  return {
+    connect,
+    disconnect,
+    initializing,
+  }
+}
---
+++ services/wallet.tsx
@@ -0,0 +1,139 @@
+import * as React from 'react'
+import { useEffect, useState } from 'react'
+import { getConfig } from '../config'
+import { createClient } from './keplr'
+import { SigningCosmWasmClient } from '@cosmjs/cosmwasm-stargate'
+import { Coin } from '@cosmjs/stargate'
+import { OfflineSigner } from '@cosmjs/proto-signing'
+
+interface CosmWasmContextType {
+  readonly initialized: boolean
+  readonly init: (signer: OfflineSigner) => void
+  readonly clear: () => void
+  readonly address: string
+  readonly balance: readonly Coin[]
+  readonly refreshBalance: () => Promise<void>
+  readonly getClient: () => SigningCosmWasmClient
+  readonly getSigner: () => OfflineSigner
+  readonly updateSigner: (singer: OfflineSigner) => void
+  readonly network: string
+  readonly setNetwork: (network: string) => void
+}
+
+function throwNotInitialized(): any {
+  throw new Error('Not yet initialized')
+}
+
+const defaultContext: CosmWasmContextType = {
+  initialized: false,
+  init: throwNotInitialized,
+  clear: throwNotInitialized,
+  address: '',
+  balance: [],
+  refreshBalance: throwNotInitialized,
+  getClient: throwNotInitialized,
+  getSigner: throwNotInitialized,
+  updateSigner: throwNotInitialized,
+  network: 'juno-mainnet',
+  setNetwork: throwNotInitialized,
+}
+
+export const CosmWasmContext =
+  React.createContext<CosmWasmContextType>(defaultContext)
+
+export const useWallet = (): CosmWasmContextType =>
+  React.useContext(CosmWasmContext)
+
+export function WalletProvider({
+  children,
+  network,
+  setNetwork,
+}: any): JSX.Element {
+  const [signer, setSigner] = useState<OfflineSigner>()
+  const [client, setClient] = useState<SigningCosmWasmClient>()
+  const config = getConfig(network)
+
+  const contextWithInit = {
+    ...defaultContext,
+    init: setSigner,
+    network,
+    setNetwork,
+  }
+  const [value, setValue] = useState<CosmWasmContextType>(contextWithInit)
+
+  const clear = (): void => {
+    setValue({ ...contextWithInit })
+    setClient(undefined)
+    setSigner(undefined)
+  }
+
+  // Get balance for each coin specified in config.coinMap
+  async function refreshBalance(
+    address: string,
+    balance: Coin[]
+  ): Promise<void> {
+    if (!client) return
+
+    balance.length = 0
+    for (const denom in config.coinMap) {
+      const coin = await client.getBalance(address, denom)
+      if (coin) balance.push(coin)
+    }
+
+    setValue({ ...value, balance })
+  }
+
+  const updateSigner = (signer: OfflineSigner) => {
+    setSigner(signer)
+  }
+
+  useEffect(() => {
+    if (!signer) return
+    ;(async function updateClient(): Promise<void> {
+      try {
+        const client = await createClient(signer, network)
+        setClient(client)
+      } catch (error) {
+        console.log(error)
+      }
+    })()
+  }, [signer])
+
+  useEffect(() => {
+    if (!signer || !client) return
+
+    const balance: Coin[] = []
+
+    ;(async function updateValue(): Promise<void> {
+      const address = (await signer.getAccounts())[0].address
+
+      await refreshBalance(address, balance)
+
+      localStorage.setItem('wallet_address', address)
+
+      setValue({
+        initialized: true,
+        init: () => {},
+        clear,
+        address,
+        balance,
+        refreshBalance: refreshBalance.bind(null, address, balance),
+        getClient: () => client,
+        getSigner: () => signer,
+        updateSigner,
+        network,
+        setNetwork,
+      })
+    })()
+  }, [client])
+
+  useEffect(() => {
+    setValue({ ...value, network })
+  }, [network])
+
+  return (
+    <CosmWasmContext.Provider value={value}>
+      {children}
+    </CosmWasmContext.Provider>
+  )
+}

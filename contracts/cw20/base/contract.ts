06827d6822030606f05f13719e70ef8ec988624e
Arda Nakışçı <anakisci@gmail.com>
2022-01-11 22:12:35 +0300

    Remove common contract file

--- contracts/cw20/base/contract.ts
+++ contracts/cw20/base/contract.ts
@@ -1,11 +1,38 @@
-import {
-  AllowanceResponse,
-  AllAllowancesResponse,
-  AllAccountsResponse,
-  SigningCosmWasmClient,
-  InstantiateResponse,
-  jsonToBinary,
-} from '../common'
+import { toUtf8, toBase64 } from '@cosmjs/encoding'
+import { SigningCosmWasmClient } from '@cosmjs/cosmwasm-stargate'
+
+export const jsonToBinary = (json: Record<string, unknown>): string => {
+  return toBase64(toUtf8(JSON.stringify(json)))
+}
+
+export type Expiration =
+  | { at_height: number }
+  | { at_time: string }
+  | { never: {} }
+
+export interface AllowanceResponse {
+  readonly allowance: string
+  readonly expires: Expiration
+}
+
+export interface AllowanceInfo {
+  readonly allowance: string
+  readonly spender: string
+  readonly expires: Expiration
+}
+
+export interface AllAllowancesResponse {
+  readonly allowances: readonly AllowanceInfo[]
+}
+
+export interface AllAccountsResponse {
+  readonly accounts: readonly string[]
+}
+
+export interface InstantiateResponse {
+  readonly contractAddress: string
+  readonly transactionHash: string
+}
 
 export interface CW20BaseInstance {
   readonly contractAddress: string

fa5d999eba48ec4418a1d4b2bf6794c7ba051b85
Arda Nakışçı <anakisci@gmail.com>
2022-01-27 11:42:22 +0300

    Add cw1 subkeys contract

---
+++ contracts/cw1/subkeys/contract.ts
@@ -0,0 +1,339 @@
+import { Coin } from '@cosmjs/proto-signing'
+import { SigningCosmWasmClient } from '@cosmjs/cosmwasm-stargate'
+
+export interface InstantiateResponse {
+  readonly contractAddress: string
+  readonly transactionHash: string
+}
+
+export type Expiration =
+  | { at_height: number }
+  | { at_time: string }
+  | { never: {} }
+
+export interface CanExecuteResponse {
+  readonly can_execute: boolean
+}
+
+export interface Permissions {
+  readonly delegate: boolean
+  readonly undelegate: boolean
+  readonly redelegate: boolean
+  readonly withdraw: boolean
+}
+
+export interface PermissionsInfo {
+  readonly spender: string
+  readonly permissions: Permissions
+}
+
+interface AllPermissionsResponse {
+  readonly permissions: readonly PermissionsInfo[]
+}
+
+export interface AllowanceInfo {
+  readonly balance: readonly Coin[]
+  readonly expires: Expiration
+  readonly spender?: string
+}
+
+export interface AllAllowancesResponse {
+  readonly allowances: readonly AllowanceInfo[]
+}
+
+interface AdminListResponse {
+  readonly admins: readonly string[]
+  readonly mutable: boolean
+}
+
+type CosmosMsg =
+  | SendMsg
+  | DelegateMsg
+  | UndelegateMsg
+  | RedelegateMsg
+  | WithdrawMsg
+
+export interface SendMsg {
+  readonly bank: {
+    readonly send: {
+      readonly from_address: string
+      readonly to_address: string
+      readonly amount: readonly Coin[]
+    }
+  }
+}
+
+export interface DelegateMsg {
+  readonly staking: {
+    readonly delegate: {
+      readonly validator: string
+      readonly amount: Coin
+    }
+  }
+}
+
+export interface UndelegateMsg {
+  readonly staking: {
+    readonly undelegate: {
+      readonly validator: string
+      readonly amount: Coin
+    }
+  }
+}
+
+export interface RedelegateMsg {
+  readonly staking: {
+    readonly redelegate: {
+      readonly src_validator: string
+      readonly dst_validator: string
+      readonly amount: Coin
+    }
+  }
+}
+
+export interface WithdrawMsg {
+  readonly staking: {
+    readonly withdraw: {
+      readonly validator: string
+      readonly recipient?: string
+    }
+  }
+}
+
+export interface CW1SubkeysInstance {
+  readonly contractAddress: string
+
+  // queries
+  admins: () => Promise<AdminListResponse>
+
+  allowance: (address?: string) => Promise<AllowanceInfo>
+
+  allAllowances: (
+    startAfter?: string,
+    limit?: number
+  ) => Promise<AllAllowancesResponse>
+
+  permissions: (address?: string) => Promise<PermissionsInfo>
+
+  allPermissions: (
+    startAfter?: string,
+    limit?: number
+  ) => Promise<AllPermissionsResponse>
+  canExecute: (sender: string, msg: CosmosMsg) => Promise<CanExecuteResponse>
+
+  // actions
+  execute: (
+    senderAddress: string,
+    msgs: readonly CosmosMsg[]
+  ) => Promise<string>
+
+  freeze: (senderAddress: string) => Promise<string>
+
+  updateAdmins: (
+    senderAddress: string,
+    admins: readonly string[]
+  ) => Promise<string>
+
+  increaseAllowance: (
+    senderAddress: string,
+    recipient: string,
+    amount: Coin,
+    expires?: Expiration
+  ) => Promise<string>
+
+  decreaseAllowance: (
+    senderAddress: string,
+    recipient: string,
+    amount: Coin,
+    expires?: Expiration
+  ) => Promise<string>
+
+  setPermissions: (
+    senderAddress: string,
+    recipient: string,
+    permissions: Permissions
+  ) => Promise<string>
+}
+
+export interface CW1SubkeysContract {
+  instantiate: (
+    senderAddress: string,
+    codeId: number,
+    initMsg: Record<string, unknown>,
+    label: string,
+    admin?: string
+  ) => Promise<InstantiateResponse>
+
+  use: (contractAddress: string) => CW1SubkeysInstance
+}
+
+export const CW1Subkeys = (
+  client: SigningCosmWasmClient
+): CW1SubkeysContract => {
+  const use = (contractAddress: string): CW1SubkeysInstance => {
+    const allowance = async (address?: string): Promise<AllowanceInfo> => {
+      return await client.queryContractSmart(contractAddress, {
+        allowance: { spender: address },
+      })
+    }
+
+    const allAllowances = async (
+      startAfter?: string,
+      limit?: number
+    ): Promise<AllAllowancesResponse> => {
+      return client.queryContractSmart(contractAddress, {
+        all_allowances: { start_after: startAfter, limit: limit },
+      })
+    }
+
+    const permissions = async (address?: string): Promise<PermissionsInfo> => {
+      return await client.queryContractSmart(contractAddress, {
+        permissions: { spender: address },
+      })
+    }
+
+    const allPermissions = async (
+      startAfter?: string,
+      limit?: number
+    ): Promise<AllPermissionsResponse> => {
+      return client.queryContractSmart(contractAddress, {
+        all_permissions: { start_after: startAfter, limit: limit },
+      })
+    }
+
+    const canExecute = async (
+      sender: string,
+      msg: CosmosMsg
+    ): Promise<CanExecuteResponse> => {
+      return client.queryContractSmart(contractAddress, {
+        can_execute: { sender: sender, msg: msg },
+      })
+    }
+
+    const admins = async (): Promise<AdminListResponse> => {
+      return client.queryContractSmart(contractAddress, { admin_list: {} })
+    }
+
+    const freeze = async (senderAddress: string): Promise<string> => {
+      const result = await client.execute(
+        senderAddress,
+        contractAddress,
+        { freeze: {} },
+        'auto'
+      )
+      return result.transactionHash
+    }
+
+    const updateAdmins = async (
+      senderAddress: string,
+      admins: readonly string[]
+    ): Promise<string> => {
+      const result = await client.execute(
+        senderAddress,
+        contractAddress,
+        { update_admins: { admins } },
+        'auto'
+      )
+      return result.transactionHash
+    }
+
+    const execute = async (
+      senderAddress: string,
+      msgs: readonly CosmosMsg[]
+    ): Promise<string> => {
+      const result = await client.execute(
+        senderAddress,
+        contractAddress,
+        { execute: { msgs } },
+        'auto'
+      )
+      return result.transactionHash
+    }
+
+    const increaseAllowance = async (
+      senderAddress: string,
+      spender: string,
+      amount: Coin,
+      expires?: Expiration
+    ): Promise<string> => {
+      const result = await client.execute(
+        senderAddress,
+        contractAddress,
+        { increase_allowance: { spender, amount, expires } },
+        'auto'
+      )
+      return result.transactionHash
+    }
+
+    const decreaseAllowance = async (
+      senderAddress: string,
+      spender: string,
+      amount: Coin,
+      expires?: Expiration
+    ): Promise<string> => {
+      const result = await client.execute(
+        senderAddress,
+        contractAddress,
+        { decrease_allowance: { spender, amount, expires } },
+        'auto'
+      )
+      return result.transactionHash
+    }
+
+    const setPermissions = async (
+      senderAddress: string,
+      spender: string,
+      permissions: Permissions
+    ): Promise<string> => {
+      const result = await client.execute(
+        senderAddress,
+        contractAddress,
+        { set_permissions: { spender, permissions } },
+        'auto'
+      )
+      return result.transactionHash
+    }
+
+    return {
+      contractAddress,
+      admins,
+      allowance,
+      allAllowances,
+      permissions,
+      allPermissions,
+      canExecute,
+      execute,
+      freeze,
+      updateAdmins,
+      increaseAllowance,
+      decreaseAllowance,
+      setPermissions,
+    }
+  }
+
+  const instantiate = async (
+    senderAddress: string,
+    codeId: number,
+    initMsg: Record<string, unknown>,
+    label: string,
+    admin?: string
+  ): Promise<InstantiateResponse> => {
+    const result = await client.instantiate(
+      senderAddress,
+      codeId,
+      initMsg,
+      label,
+      'auto',
+      {
+        memo: 'label',
+        admin,
+      }
+    )
+    return {
+      contractAddress: result.contractAddress,
+      transactionHash: result.transactionHash,
+    }
+  }
+
+  return { use, instantiate }
+}
---
+++ contracts/cw1/subkeys/useContract.ts
@@ -0,0 +1,74 @@
+import { useCallback, useEffect, useState } from 'react'
+import { useWallet } from 'contexts/wallet'
+import {
+  CW1SubkeysContract,
+  CW1SubkeysInstance,
+  CW1Subkeys as initContract,
+} from './contract'
+
+interface InstantiateResponse {
+  readonly contractAddress: string
+  readonly transactionHash: string
+}
+
+export interface UseCW1SubkeysContractProps {
+  instantiate: (
+    codeId: number,
+    initMsg: Record<string, unknown>,
+    label: string,
+    admin?: string
+  ) => Promise<InstantiateResponse>
+  use: (customAddress: string) => CW1SubkeysInstance | undefined
+  updateContractAddress: (contractAddress: string) => void
+}
+
+export function useCW20BaseContract(): UseCW1SubkeysContractProps {
+  const wallet = useWallet()
+
+  const [address, setAddress] = useState<string>('')
+  const [CW1Subkeys, setCW1Subkeys] = useState<CW1SubkeysContract>()
+
+  useEffect(() => {
+    setAddress(localStorage.getItem('contract_address') || '')
+  }, [])
+
+  useEffect(() => {
+    if (wallet.initialized) {
+      const getCW20BaseInstance = async (): Promise<void> => {
+        const cw20BaseContract = initContract(wallet.getClient())
+        setCW1Subkeys(cw20BaseContract)
+      }
+
+      getCW20BaseInstance()
+    }
+  }, [wallet])
+
+  const updateContractAddress = (contractAddress: string) => {
+    setAddress(contractAddress)
+  }
+
+  const instantiate = useCallback(
+    (codeId, initMsg, label, admin?): Promise<InstantiateResponse> => {
+      return new Promise((resolve, reject) => {
+        if (!CW1Subkeys) return reject('Contract is not initialized.')
+        CW1Subkeys.instantiate(wallet.address, codeId, initMsg, label, admin)
+          .then(resolve)
+          .catch(reject)
+      })
+    },
+    [CW1Subkeys, wallet]
+  )
+
+  const use = useCallback(
+    (customAddress = ''): CW1SubkeysInstance | undefined => {
+      return CW1Subkeys?.use(address || customAddress)
+    },
+    [CW1Subkeys, address]
+  )
+
+  return {
+    instantiate,
+    use,
+    updateContractAddress,
+  }
+}

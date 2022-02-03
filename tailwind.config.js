2acd85c8485586d6fe142b153d9c9012248177eb
Griko Nibras <git@griko.id>
2022-02-04 04:45:29 +0700

    refactor(tailwind): replace inter with roboto
    Signed-off-by: Griko Nibras <git@griko.id>

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
         purple: '#7E5DFF',
       },
       fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        sans: ['Roboto', ...defaultTheme.fontFamily.sans],
         mono: ['"JetBrains Mono"', ...defaultTheme.fontFamily.mono],
       },
     },

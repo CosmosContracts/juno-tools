a831c233ec05589822b762467662b8f132c41e43
Arda Nakışçı <anakisci@gmail.com>
2022-02-03 11:39:56 +0300

    Design changes

--- pages/airdrops/actions.tsx
+++ pages/airdrops/actions.tsx
@@ -2,15 +2,22 @@ import React from 'react'
 import type { NextPage } from 'next'
 import Link from 'next/link'
 import { ImArrowRight2 } from 'react-icons/im'
+import { useTheme } from 'contexts/theme'
 
 const Home: NextPage = () => {
+  const theme = useTheme()
+
   return (
     <div className="h-3/4 w-3/4">
-      <h1 className="text-6xl font-bold">Merkle Drop</h1>
+      <h1 className="text-6xl font-bold text-center">Airdrop Creation</h1>
 
-      <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 max-w-full sm:w-full">
+      <div
+        className={`flex flex-wrap items-center justify-around mt-6 max-w-full ${
+          theme.isDarkTheme ? 'border-gray/20' : 'border-dark/20'
+        }`}
+      >
         <Link href="/airdrops/create" passHref>
-          <button className="p-6 mt-6 text-left border border-secondary hover:border-primary w-96 rounded-xl hover:text-primary focus:text-primary-focus">
+          <button className="p-6 mt-6 text-left border w-96 rounded-xl focus:text-primary-focus">
             <h3 className="text-2xl font-bold">
               Create a New Airdrop
               <ImArrowRight2 className="ml-3" />
@@ -19,9 +26,13 @@ const Home: NextPage = () => {
         </Link>
       </div>
 
-      <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 max-w-full sm:w-full">
+      <div
+        className={`flex flex-wrap items-center justify-around mt-6 max-w-full ${
+          theme.isDarkTheme ? 'border-gray/20' : 'border-dark/20'
+        }`}
+      >
         <Link href="/airdrops/register" passHref>
-          <button className="p-6 mt-6 text-left border border-secondary hover:border-primary w-96 rounded-xl hover:text-primary focus:text-primary-focus">
+          <button className="p-6 mt-6 text-left border w-96 rounded-xl focus:text-primary-focus">
             <h3 className="text-2xl font-bold">
               Register an Airdrop
               <ImArrowRight2 className="ml-3" />
@@ -30,9 +41,13 @@ const Home: NextPage = () => {
         </Link>
       </div>
 
-      <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 max-w-full sm:w-full">
+      <div
+        className={`flex flex-wrap items-center justify-around mt-6 max-w-full ${
+          theme.isDarkTheme ? 'border-gray/20' : 'border-dark/20'
+        }`}
+      >
         <Link href="/airdrops/fund" passHref>
-          <button className="p-6 mt-6 text-left border border-secondary hover:border-primary w-96 rounded-xl hover:text-primary focus:text-primary-focus">
+          <button className="p-6 mt-6 text-left border w-96 rounded-xl focus:text-primary-focus">
             <h3 className="text-2xl font-bold">
               Send Funds to an Airdrop
               <ImArrowRight2 className="ml-3" />
--- pages/airdrops/index.tsx
+++ pages/airdrops/index.tsx
@@ -1,36 +1,47 @@
+import { useTheme } from 'contexts/theme'
 import { NextPage } from 'next'
 import Link from 'next/link'
 import { ImArrowRight2 } from 'react-icons/im'
 
 const Airdrop: NextPage = () => {
+  const theme = useTheme()
+
   return (
     <div className="h-3/4 w-3/4">
       <h1 className="text-6xl font-bold text-center">Airdrop Tokens</h1>
 
       <div className="mt-5 text-center text-lg">
-        Looking for a fast and efficient way to airdrop your minted tokens or
-        come to claim your allocation? <br />
-        Use our airdrop tool to create/claim your tokens!
+        Looking for a fast and efficient way to airdrop your project or come to
+        claim your allocation? <br />
+        Use our airdrop tool to create/claim your airdrop!
       </div>
 
       <div className="flex items-center justify-evenly h-3/4">
         <Link href="/airdrops/list" passHref>
-          <button>
-            <div className="h-32 p-6 rounded-xl">
+          <button
+            className={`border rounded-xl ${
+              theme.isDarkTheme ? 'border-gray/20' : 'border-dark/20'
+            }`}
+          >
+            <div className="h-32 p-6">
               <div className="flex items-center text-3xl font-bold mb-2">
                 Available Airdrops <ImArrowRight2 className="ml-3" />
               </div>
-              View airdrops you would like to claim
+              View airdrops you would like to claim.
             </div>
           </button>
         </Link>
         <Link href="/airdrops/actions" passHref>
-          <button>
-            <div className="h-32 p-6 rounded-xl">
+          <button
+            className={`border rounded-xl ${
+              theme.isDarkTheme ? 'border-gray/20' : 'border-dark/20'
+            }`}
+          >
+            <div className="h-32 p-6">
               <div className="flex items-center text-3xl font-bold mb-2">
                 Create a New Airdrop <ImArrowRight2 className="ml-3" />
               </div>
-              Register your CW20 tokens for airdrop
+              Register your CW20 tokens for airdrop.
             </div>
           </button>
         </Link>

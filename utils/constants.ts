deeb5fc5ef7506d76a6aeeee5f17591bb6dc07c6
findolor <anakisci@gmail.com>
2022-02-05 14:54:35 +0300

    Add network identifier to code id variables

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,25 +1,50 @@
-export const CW20_MERKLE_DROP_CODE_ID = parseInt(
-  process.env.NEXT_PUBLIC_CW20_MERKLE_DROP_CODE_ID as string,
+export const TESTNET_CW20_MERKLE_DROP_CODE_ID = parseInt(
+  process.env.NEXT_PUBLIC_TESTNET_CW20_MERKLE_DROP_CODE_ID as string,
   10
 )
 
-export const CW20_BASE_CODE_ID = parseInt(
-  process.env.NEXT_PUBLIC_CW20_BASE_CODE_ID as string,
+export const TESTNET_CW20_BASE_CODE_ID = parseInt(
+  process.env.NEXT_PUBLIC_TESTNET_CW20_BASE_CODE_ID as string,
   10
 )
 
-export const CW20_BONDING_CODE_ID = parseInt(
-  process.env.NEXT_PUBLIC_CW20_BONDING_CODE_ID as string,
+export const TESTNET_CW20_BONDING_CODE_ID = parseInt(
+  process.env.NEXT_PUBLIC_TESTNET_CW20_BONDING_CODE_ID as string,
   10
 )
 
-export const CW20_STAKING_CODE_ID = parseInt(
-  process.env.NEXT_PUBLIC_CW20_STAKING_CODE_ID as string,
+export const TESTNET_CW20_STAKING_CODE_ID = parseInt(
+  process.env.NEXT_PUBLIC_TESTNET_CW20_STAKING_CODE_ID as string,
   10
 )
 
-export const CW1_SUBKEYS_CODE_ID = parseInt(
-  process.env.NEXT_PUBLIC_CW1_SUBKEYS_CODE_ID as string,
+export const TESTNET_CW1_SUBKEYS_CODE_ID = parseInt(
+  process.env.NEXT_PUBLIC_TESTNET_CW1_SUBKEYS_CODE_ID as string,
+  10
+)
+
+export const MAINNET_CW20_MERKLE_DROP_CODE_ID = parseInt(
+  process.env.NEXT_PUBLIC_MAINNET_CW20_MERKLE_DROP_CODE_ID as string,
+  10
+)
+
+export const MAINNET_CW20_BASE_CODE_ID = parseInt(
+  process.env.NEXT_PUBLIC_MAINNET_CW20_BASE_CODE_ID as string,
+  10
+)
+
+export const MAINNET_CW20_BONDING_CODE_ID = parseInt(
+  process.env.NEXT_PUBLIC_MAINNET_CW20_BONDING_CODE_ID as string,
+  10
+)
+
+export const MAINNET_CW20_STAKING_CODE_ID = parseInt(
+  process.env.NEXT_PUBLIC_MAINNET_CW20_STAKING_CODE_ID as string,
+  10
+)
+
+export const MAINNET_CW1_SUBKEYS_CODE_ID = parseInt(
+  process.env.NEXT_PUBLIC_MAINNET_CW1_SUBKEYS_CODE_ID as string,
   10
 )
 

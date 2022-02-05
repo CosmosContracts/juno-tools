6c16862ad9612be6ce1cb8be142f9a2fcd14846c
findolor <anakisci@gmail.com>
2022-02-05 15:00:07 +0300

    Remove wallet components from landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,16 +19,6 @@ const Home: NextPage = () => {
       <main className={styles.main}>
         <h1 className={styles.title}>Welcome to Juno Tools!</h1>
 
-        <div className={styles.grid}>
-          <button className={styles.card} onClick={connectWallet}>
-            <h2>Connect Wallet &rarr;</h2>
-          </button>
-
-          <button className={styles.card} onClick={disconnectWallet}>
-            <h2>Disconnect Wallet &rarr;</h2>
-          </button>
-        </div>
-
         {wallet.initialized && <div>{wallet.address}</div>}
         {wallet.initialized && <div>{JSON.stringify(wallet.balance)}</div>}
       </main>

c06027d70a3e00192eab1710a7087452d3492f9f
findolor <anakisci@gmail.com>
2022-02-06 18:38:06 +0300

    Change component directions

--- pages/airdrops/actions.tsx
+++ pages/airdrops/actions.tsx
@@ -11,49 +11,51 @@ const Home: NextPage = () => {
     <div className="h-3/4 w-3/4">
       <h1 className="text-6xl font-bold text-center">Airdrop Creation</h1>
 
-      <div
-        className={`flex flex-wrap items-center justify-around mt-6 max-w-full ${
-          theme.isDarkTheme ? 'border-gray/20' : 'border-dark/20'
-        }`}
-      >
-        <Link href="/airdrops/create" passHref>
-          <button className="p-6 mt-6 text-left border w-96 rounded-xl">
-            <h3 className="text-2xl font-bold">
-              Create a New Airdrop
-              <ImArrowRight2 className="ml-3" />
-            </h3>
-          </button>
-        </Link>
-      </div>
+      <div className="flex h-full justify-evenly">
+        <div
+          className={`flex flex-wrap items-center justify-around mt-6 max-w-full ${
+            theme.isDarkTheme ? 'border-gray/20' : 'border-dark/20'
+          }`}
+        >
+          <Link href="/airdrops/create" passHref>
+            <button className="flex justify-center p-6 mt-6 text-left border w-72 rounded-xl">
+              <h3 className="flex items-center text-2xl font-bold">
+                Create Airdrop
+                <ImArrowRight2 className="ml-3" />
+              </h3>
+            </button>
+          </Link>
+        </div>
 
-      <div
-        className={`flex flex-wrap items-center justify-around mt-6 max-w-full ${
-          theme.isDarkTheme ? 'border-gray/20' : 'border-dark/20'
-        }`}
-      >
-        <Link href="/airdrops/register" passHref>
-          <button className="p-6 mt-6 text-left border w-96 rounded-xl">
-            <h3 className="text-2xl font-bold">
-              Register an Airdrop
-              <ImArrowRight2 className="ml-3" />
-            </h3>
-          </button>
-        </Link>
-      </div>
+        <div
+          className={`flex flex-wrap items-center justify-around mt-6 max-w-full ${
+            theme.isDarkTheme ? 'border-gray/20' : 'border-dark/20'
+          }`}
+        >
+          <Link href="/airdrops/register" passHref>
+            <button className="flex justify-center p-6 mt-6 text-left border w-72 rounded-xl">
+              <h3 className="flex items-center text-2xl font-bold">
+                Register Airdrop
+                <ImArrowRight2 className="ml-3" />
+              </h3>
+            </button>
+          </Link>
+        </div>
 
-      <div
-        className={`flex flex-wrap items-center justify-around mt-6 max-w-full ${
-          theme.isDarkTheme ? 'border-gray/20' : 'border-dark/20'
-        }`}
-      >
-        <Link href="/airdrops/fund" passHref>
-          <button className="p-6 mt-6 text-left border w-96 rounded-xl">
-            <h3 className="text-2xl font-bold">
-              Send Funds to an Airdrop
-              <ImArrowRight2 className="ml-3" />
-            </h3>
-          </button>
-        </Link>
+        <div
+          className={`flex flex-wrap items-center justify-around mt-6 max-w-full ${
+            theme.isDarkTheme ? 'border-gray/20' : 'border-dark/20'
+          }`}
+        >
+          <Link href="/airdrops/fund" passHref>
+            <button className="flex justify-center p-6 mt-6 text-left border w-72 rounded-xl">
+              <h3 className="flex items-center text-2xl font-bold">
+                Fund Airdrop
+                <ImArrowRight2 className="ml-3" />
+              </h3>
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   )

77dd026507d0a1e1cfc29dbe4631ad23efe08691
findolor <anakisci@gmail.com>
2022-02-08 16:07:52 +0300

    Add more err messages in airdrop file validation

--- utils/isValidAirdropFile.ts
+++ utils/isValidAirdropFile.ts
@@ -1,3 +1,4 @@
+import toast from 'react-hot-toast'
 import { isValidAddress } from './isValidAddress'
 
 interface AccountProps {
@@ -26,22 +27,48 @@ const isValidAirdropFile = (file: AirdropFileProps) => {
     !file.hasOwnProperty('expiration') ||
     !file.hasOwnProperty('expirationType') ||
     !file.hasOwnProperty('totalAmount')
-  )
+  ) {
+    toast.error('Missing properties in airdrop file')
     return false
-  if (file.accounts.length > 15000) return false
+  }
+  if (file.accounts.length > 15000) {
+    toast.error('Accounts has to be less than 15000 addresses')
+    return false
+  }
   if (
     file.startType !== 'timestamp' &&
     file.startType !== 'height' &&
     file.startType !== null
-  )
+  ) {
+    toast.error('Start type must be timestamp or height or null')
     return false
+  }
   if (
     file.expirationType !== 'timestamp' &&
     file.expirationType !== 'height' &&
     file.expirationType !== null
-  )
+  ) {
+    toast.error('Expiration Type must be timestamp or height or null')
     return false
-  if (!isValidAddress(file.cw20TokenAddress)) return false
+  }
+  if (
+    (file.startType === null && file.start !== null) ||
+    (file.startType !== null && file.start === null)
+  ) {
+    toast.error('Start and start type must be set together')
+    return false
+  }
+  if (
+    (file.expirationType === null && file.expiration !== null) ||
+    (file.expirationType !== null && file.expiration === null)
+  ) {
+    toast.error('Expiration and expiration type must be set together')
+    return false
+  }
+  if (!isValidAddress(file.cw20TokenAddress)) {
+    toast.error('Invalid cw20 token address')
+    return false
+  }
 
   return true
 }

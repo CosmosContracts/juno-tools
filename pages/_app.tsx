4df307635b728c6118129dcdaad014daa5af630d
Griko Nibras <git@griko.id>
2022-02-14 17:16:10 +0700

    feat(pages): register wallet loader side effects

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,32 @@
 import '@fontsource/jetbrains-mono/latin.css'
 import '@fontsource/roboto/latin.css'
-
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { WalletProvider } from 'contexts/wallet'
+
+import Layout from 'components/Layout'
 import { ContractsProvider } from 'contexts/contracts'
 import { ThemeProvider } from 'contexts/theme'
-import Layout from 'components/Layout'
+import { WalletProvider } from 'contexts/wallet'
+import type { AppProps } from 'next/app'
+import { useEffect, useState } from 'react'
 import { Toaster } from 'react-hot-toast'
-import { useState } from 'react'
+import { useKeplr } from 'services/keplr'
 import { NETWORK } from 'utils/constants'
 
-function MyApp({ Component, pageProps }: AppProps) {
+const SideEffects = () => {
+  const keplr = useKeplr()
+
+  useEffect(() => {
+    const listenKeystoreChange = () => keplr.connect(true)
+    window.addEventListener('keplr_keystorechange', listenKeystoreChange)
+    return () => {
+      window.removeEventListener('keplr_keystorechange', listenKeystoreChange)
+    }
+  }, [keplr])
+
+  return null
+}
+
+export default function App({ Component, pageProps }: AppProps) {
   const [isDarkTheme, setIsDarkTheme] = useState(true)
   const [network, setNetwork] = useState(NETWORK)
 
@@ -23,10 +38,9 @@ function MyApp({ Component, pageProps }: AppProps) {
           <Layout>
             <Component {...pageProps} />
           </Layout>
+          <SideEffects />
         </ContractsProvider>
       </WalletProvider>
     </ThemeProvider>
   )
 }
-
-export default MyApp

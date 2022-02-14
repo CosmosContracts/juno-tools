82cb75ad468c17e52e557a98d97f1514cf2e1fdb
findolor <anakisci@gmail.com>
2022-02-14 11:52:22 +0300

    Linter

--- utils/isValidAccountsFile.ts
+++ utils/isValidAccountsFile.ts
@@ -8,30 +8,32 @@ export interface AccountProps {
 }
 
 export const isValidAccountsFile = (file: Array<AccountProps>) => {
-  const checks = file.map(account => {
+  const checks = file.map((account) => {
     // Check if fields are valid
-    if(!account.hasOwnProperty('address') || !account.hasOwnProperty('amount')) {
+    if (
+      !account.hasOwnProperty('address') ||
+      !account.hasOwnProperty('amount')
+    ) {
       return { field: false }
     }
     // Check if address is valid bech32 address
-    if(!isValidAddress(account.address)) {
+    if (!isValidAddress(account.address)) {
       return { address: false }
     }
     // Check if address start with juno
-    if(account.address.slice(0, 4) !== 'juno') {
+    if (account.address.slice(0, 4) !== 'juno') {
       return { address: false }
     }
   })
 
-  if(checks.filter(check => check?.field === false).length > 0) {
+  if (checks.filter((check) => check?.field === false).length > 0) {
     toast.error('Invalid accounts file')
     return false
   }
-  if(checks.filter(check => check?.address === false).length > 0) {
+  if (checks.filter((check) => check?.address === false).length > 0) {
     toast.error('Invalid address in file')
     return false
   }
 
   return true
 }
-

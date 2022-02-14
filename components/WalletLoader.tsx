e5ab266df52d379771156c1d82e158a88bc25b2d
Griko Nibras <git@griko.id>
2022-02-14 17:19:30 +0700

    refactor(ui): rework wallet loader component

--- components/WalletLoader.tsx
+++ components/WalletLoader.tsx
@@ -1,15 +1,42 @@
-import { NextPage } from 'next'
-import { useCallback } from 'react'
+import clsx from 'clsx'
+import { useWallet } from 'contexts/wallet'
+import { BiWallet } from 'react-icons/bi'
+import { FaSpinner } from 'react-icons/fa'
 import { useKeplr } from 'services/keplr'
+import getShortAddress from 'utils/getShortAddress'
 
-const WalletLoader: NextPage = () => {
+const WalletLoader = () => {
   const keplr = useKeplr()
+  const wallet = useWallet()
 
-  const connectWallet = useCallback(() => keplr.connect(), [keplr])
+  const handleClick = () => {
+    return (wallet.initialized ? keplr.disconnect : keplr.connect)(true)
+  }
 
   return (
-    <button onClick={connectWallet}>
-      <h2>Connect Wallet &rarr;</h2>
+    <button
+      className={clsx(
+        'flex gap-x-2 items-center text-sm font-bold uppercase truncate', // content styling
+        'py-2 px-4 my-8 border border-plumbus', // button styling
+        'hover:bg-white/10 transition-colors', // hover styling
+        { 'cursor-wait': keplr.initializing } // loading styling
+      )}
+      disabled={keplr.initializing}
+      onClick={handleClick}
+    >
+      {keplr.initializing ? (
+        <FaSpinner size={16} className="animate-spin" />
+      ) : (
+        <BiWallet size={16} />
+      )}
+
+      <span>
+        {wallet.initialized
+          ? wallet.name || getShortAddress(wallet.address)
+          : keplr.initializing
+          ? 'Loading...'
+          : 'Connect Wallet'}
+      </span>
     </button>
   )
 }

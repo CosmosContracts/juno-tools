fc4ad31fca39a6203bd15680a282be96062645b2
Griko Nibras <git@griko.id>
2022-02-17 01:54:57 +0700

    refactor(ui): simplify sidebar structure

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,9 +5,7 @@ import { useRouter } from 'next/router'
 import { footerLinks, links, socialsLinks } from 'utils/links'
 
 import Anchor from './Anchor'
-import SidebarContainer from './SidebarContainer'
 import SidebarLayout from './SidebarLayout'
-import SidebarToggle from './SidebarToggle'
 import WalletLoader from './WalletLoader'
 
 const routes = [
@@ -22,71 +20,62 @@ const Sidebar = () => {
 
   return (
     <SidebarLayout>
-      <SidebarToggle />
-      <SidebarContainer>
-        {/* juno brand as home button */}
-        <Anchor href="/">
-          <Brand className="w-24" />
+      {/* juno brand as home button */}
+      <Anchor href="/">
+        <Brand className="w-2/4" />
+      </Anchor>
+
+      {/* wallet button */}
+      <WalletLoader />
+
+      {/* main navigation routes */}
+      {routes.map(({ text, href }) => (
+        <Anchor
+          href={href}
+          key={href}
+          className={clsx(
+            'py-2 px-4 -mx-4 uppercase', // styling
+            'hover:bg-white/5 transition-colors', // hover styling
+            { 'font-bold text-plumbus': router.asPath.startsWith(href) } // active route styling
+          )}
+        >
+          {text}
         </Anchor>
+      ))}
 
-        {/* wallet button */}
-        <WalletLoader />
+      <div className="flex-grow" />
 
-        {/* main navigation routes */}
-        {routes.map(({ text, href }) => (
-          <Anchor
-            href={href}
-            key={href}
-            className={clsx(
-              'py-2 px-4 -mx-4 uppercase', // styling
-              'hover:bg-white/5 transition-colors', // hover styling
-              { 'font-bold text-plumbus': router.asPath.startsWith(href) } // active route styling
-            )}
-          >
-            {text}
+      {/* juno network status */}
+      <div className="text-sm">Network: {wallet.network}</div>
+
+      {/* footer reference links */}
+      <ul className="text-sm list-disc list-inside">
+        {footerLinks.map(({ href, text }) => (
+          <li key={href}>
+            <Anchor href={href} className="hover:text-plumbus hover:underline">
+              {text}
+            </Anchor>
+          </li>
+        ))}
+      </ul>
+
+      {/* footer attribution */}
+      <div className="text-xs text-white/50">
+        JunoTools 1.0. Copyright &copy; {new Date().getFullYear()} by{' '}
+        <Anchor href={links.deuslabs} className="text-plumbus hover:underline">
+          deus labs
+        </Anchor>
+        . All rights reserved.
+      </div>
+
+      {/* footer social links */}
+      <div className="flex gap-x-6 items-center text-white/75">
+        {socialsLinks.map(({ Icon, href, text }) => (
+          <Anchor href={href} key={href} className="hover:text-plumbus">
+            <Icon aria-label={text} size={20} />
           </Anchor>
         ))}
-
-        <div className="flex-grow" />
-
-        {/* juno network status */}
-        <div className="text-sm">Network: {wallet.network}</div>
-
-        {/* footer reference links */}
-        <ul className="text-sm list-disc list-inside">
-          {footerLinks.map(({ href, text }) => (
-            <li key={href}>
-              <Anchor
-                href={href}
-                className="hover:text-plumbus hover:underline"
-              >
-                {text}
-              </Anchor>
-            </li>
-          ))}
-        </ul>
-
-        {/* footer attribution */}
-        <div className="text-xs text-white/50">
-          JunoTools 1.0. Copyright &copy; {new Date().getFullYear()} by{' '}
-          <Anchor
-            href={links.deuslabs}
-            className="text-plumbus hover:underline"
-          >
-            deus labs
-          </Anchor>
-          . All rights reserved.
-        </div>
-
-        {/* footer social links */}
-        <div className="flex gap-x-6 items-center text-white/75">
-          {socialsLinks.map(({ Icon, href, text }) => (
-            <Anchor href={href} key={href} className="hover:text-plumbus">
-              <Icon aria-label={text} size={20} />
-            </Anchor>
-          ))}
-        </div>
-      </SidebarContainer>
+      </div>
     </SidebarLayout>
   )
 }
--- components/SidebarLayout.tsx
+++ components/SidebarLayout.tsx
@@ -1,6 +1,7 @@
 import clsx from 'clsx'
-import { useSidebarStore } from 'contexts/sidebar'
+import { toggleSidebar, useSidebarStore } from 'contexts/sidebar'
 import { PropsWithChildren } from 'react'
+import { FaChevronLeft } from 'react-icons/fa'
 
 const SidebarLayout = ({ children }: PropsWithChildren<{}>) => {
   const { isOpen } = useSidebarStore()
@@ -8,21 +9,42 @@ const SidebarLayout = ({ children }: PropsWithChildren<{}>) => {
   return (
     <div
       className={clsx(
-        isOpen ? 'min-w-[250px] max-w-[250px]' : 'min-w-[20px] max-w-[20px]', // layout width
-        'relative z-10' // layout positioning
+        isOpen ? 'min-w-[250px] max-w-[250px]' : 'min-w-[20px] max-w-[20px]',
+        'relative z-10'
       )}
     >
+      {/* fixed component */}
       <div
         className={clsx(
-          'fixed top-0 left-0', // anchor layout
-          'min-w-[250px] max-w-[250px]', // actual sidebar width
-          'bg-black/50 border-r-[1px] border-r-plumbus-light', // background and border
-          'transition-transform ease-out', // animation transition
-          { 'translate-x-[-220px]': !isOpen } // hidden state
+          'overflow-auto fixed top-0 left-0 min-w-[250px] max-w-[250px] no-scrollbar',
+          'bg-black/50 border-r-[1px] border-r-plumbus-light',
+          { 'translate-x-[-230px]': !isOpen }
         )}
       >
-        {children}
+        {/* inner component */}
+        <div
+          className={clsx('flex flex-col gap-y-4 p-8 min-h-screen', {
+            invisible: !isOpen,
+          })}
+        >
+          {children}
+        </div>
       </div>
+
+      {/* sidebar toggle */}
+      <button
+        className={clsx(
+          'absolute top-[32px] right-[-12px] p-1 w-[24px] h-[24px]',
+          'text-black bg-plumbus-light rounded-full',
+          'hover:bg-plumbus'
+        )}
+        onClick={toggleSidebar}
+      >
+        <FaChevronLeft
+          size={12}
+          className={clsx('mx-auto', { 'rotate-180': !isOpen })}
+        />
+      </button>
     </div>
   )
 }

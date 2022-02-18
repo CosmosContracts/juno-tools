62eaad04493425e91ec7e6bce4457ab172ead6f0
Griko Nibras <git@griko.id>
2022-02-19 04:01:10 +0700

    feat(config): add meta.ts

---
+++ config/meta.ts
@@ -0,0 +1,11 @@
+const meta = {
+  name: 'JunoTools',
+  description: `JunoTools is a swiss knife that helps you build on Juno by providing smart contract front ends`,
+  domain: 'juno.tools',
+  url: 'https://juno.tools',
+  twitter: {
+    username: '@junotools',
+  },
+}
+
+export default meta

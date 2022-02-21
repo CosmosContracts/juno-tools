740aff2fda877f55e5e540c37a2a925c025e316e
Griko Nibras <git@griko.id>
2022-02-22 02:20:03 +0700

    refactor(config): restructure next config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,35 @@
-const withTM = require('next-transpile-modules')(['react-syntax-highlighter'])
+// @ts-check
 
-module.exports = withTM({
+const withTranspileModules = require('next-transpile-modules')
+
+/**
+ * @type {import("next").NextConfig}
+ * @see https://nextjs.org/docs/api-reference/next.config.js/introduction
+ */
+let nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
-  webpack(config) {
+  webpack(config, { dev, webpack }) {
+    // svgr integration
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
       use: ['@svgr/webpack'],
     })
+
     return config
   },
-})
+}
+
+// include local modules
+nextConfig = withTranspileModules(
+  [
+    'react-syntax-highlighter',
+    //
+  ],
+  {
+    debug: Boolean(process.env.DEBUG),
+  }
+)(nextConfig)
+
+module.exports = nextConfig

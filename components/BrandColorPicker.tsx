37b8cdb7f0a55b2170b497cf0ce73df038b6afd7
Griko Nibras <git@griko.id>
2022-03-05 04:10:32 +0700

    feat(ui): create brand preview related components

---
+++ components/BrandColorPicker.tsx
@@ -0,0 +1,34 @@
+import clsx from 'clsx'
+import { Dispatch, SetStateAction } from 'react'
+
+export const BRAND_COLORS = ['plumbus', 'black', 'white'] as const
+export type BrandColor = typeof BRAND_COLORS[number]
+
+export interface BrandColorPickerProps {
+  onChange: Dispatch<SetStateAction<BrandColor>>
+}
+
+const BrandColorPicker = ({ onChange }: BrandColorPickerProps) => {
+  return (
+    <div className="flex items-center space-x-2">
+      <span className="text-sm font-bold">Change color:</span>
+      {BRAND_COLORS.map((color) => (
+        <button
+          key={`change-color-${color}`}
+          className={clsx(
+            'w-8 h-8 rounded border border-white/20',
+            'hover:ring-2 focus:ring-2 ring-white/50 transition',
+            {
+              'bg-plumbus': color == 'plumbus',
+              'bg-black': color == 'black',
+              'bg-white': color == 'white',
+            }
+          )}
+          onClick={() => onChange(color)}
+        />
+      ))}
+    </div>
+  )
+}
+
+export default BrandColorPicker

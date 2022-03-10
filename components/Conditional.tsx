d8947797b99d21925ec2ab6d49fe850af55c9711
Griko Nibras <git@griko.id>
2022-03-11 04:26:02 +0700

    chore(ui): loosen conditional component test prop

--- components/Conditional.tsx
+++ components/Conditional.tsx
@@ -1,12 +1,12 @@
 import { ReactNode } from 'react'
 
 export interface ConditionalProps {
-  test: boolean
+  test: any
   children: ReactNode
 }
 
 const Conditional = ({ test, children }: ConditionalProps) => {
-  if (!test) return null
+  if (!Boolean(test)) return null
   return <>{children}</>
 }
 

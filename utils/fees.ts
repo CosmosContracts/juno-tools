215e9f7981735283548da2a73e55053c4b8e71ad
findolor <anakisci@gmail.com>
2022-03-10 15:01:41 +0300

    Change fee

--- utils/fees.ts
+++ utils/fees.ts
@@ -5,7 +5,10 @@ import { NETWORK } from './constants'
 
 const getExecuteFee = () => {
   const config = getConfig(NETWORK)
-  return coins(50000, config.feeToken)
+  return {
+    amount: coins(500000, config.feeToken),
+    gas: '1000000',
+  }
 }
 
 export default getExecuteFee

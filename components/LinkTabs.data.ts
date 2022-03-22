18b1eb6af46d5fc32a638ff4dd000aaf19f45630
Griko Nibras <git@griko.id>
2022-03-23 02:54:46 +0700

    feat(ui): create link tabs components

---
+++ components/LinkTabs.data.ts
@@ -0,0 +1,19 @@
+import { LinkTabProps } from './LinkTab'
+
+export const cw20LinkTabs: LinkTabProps[] = [
+  {
+    title: 'Instantiate',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.`,
+    href: '/contracts/cw20/instantiate',
+  },
+  {
+    title: 'Query',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.`,
+    href: '/contracts/cw20/query',
+  },
+  {
+    title: 'Execute',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.`,
+    href: '/contracts/cw20/execute',
+  },
+]

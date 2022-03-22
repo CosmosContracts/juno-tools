e00fd3a8560b2468aaa3270ae4b296c9e4019bb5
Griko Nibras <git@griko.id>
2022-03-23 03:49:14 +0700

    refactor(pages): rework airdrop claim page

--- pages/airdrops/[address]/claim.tsx
+++ pages/airdrops/[address]/claim.tsx
@@ -1,22 +1,43 @@
 import axios from 'axios'
-import JsonPreview from 'components/JsonPreview'
+import clsx from 'clsx'
+import Alert from 'components/Alert'
+import StackedList from 'components/StackedList'
 import { useContracts } from 'contexts/contracts'
 import { useWallet } from 'contexts/wallet'
+import { GetServerSideProps, NextPage } from 'next'
 import { useRouter } from 'next/router'
 import { NextSeo } from 'next-seo'
 import { useEffect, useState } from 'react'
 import toast from 'react-hot-toast'
+import { CgSpinnerAlt } from 'react-icons/cg'
+import { FaAsterisk } from 'react-icons/fa'
+import { withMetadata } from 'utils/layout'
 
-const ClaimDrop = ({ address }: { address: string }) => {
+interface ClaimAirdropPageProps {
+  contractAddress: string
+}
+
+export const getServerSideProps: GetServerSideProps<
+  ClaimAirdropPageProps
+> = async ({ params }) => {
+  return {
+    props: {
+      contractAddress: String(params?.address),
+    },
+  }
+}
+
+const ClaimAirdropPage: NextPage<ClaimAirdropPageProps> = ({
+  contractAddress,
+}) => {
   const router = useRouter()
-  const contractAddress = address
   const wallet = useWallet()
   const cw20MerkleAirdropContract = useContracts().cw20MerkleAirdrop
   const cw20BaseContract = useContracts().cw20Base
 
   const [amount, setAmount] = useState('')
   const [loading, setLoading] = useState(false)
-  const [proofs, setProofs] = useState<[string]>([''])
+  const [proofs, setProofs] = useState<string[]>([''])
   const [name, setName] = useState('')
   const [cw20TokenAddress, setCW20TokenAddress] = useState('')
   const [balance, setBalance] = useState(0)
@@ -88,44 +109,83 @@ const ClaimDrop = ({ address }: { address: string }) => {
     }
   }
 
-  if (!wallet.initialized) return <div>Please connect your wallet!</div>
-
   return (
-    <div className="w-3/4 h-3/4">
+    <section className="relative py-6 px-12 space-y-8">
       <NextSeo title="Claim Airdrop" />
 
-      <h1 className="mb-20 text-6xl font-bold text-center">{name}</h1>
+      <div className="space-y-4">
+        <h1 className="font-heading text-4xl font-bold">Claim Airdrop</h1>
+        {!wallet.initialized && (
+          <Alert type="warning">
+            No wallet detected. Please connect your wallet before claiming an
+            airdrop.
+          </Alert>
+        )}
+        {wallet.initialized && (
+          <p>
+            Click the claim airdrop button below to add{' '}
+            <b>{amount ?? '...'} juno</b> to your total balance.
+            <br />
+            Your current total token balance is <b>{balance / 1000000} juno</b>.
+          </p>
+        )}
+      </div>
 
-      <h1 className="mb-10 text-3xl font-bold text-center">
-        Your airdrop allocation: {amount} {}
-      </h1>
-      <h1 className="mb-10 text-3xl font-bold text-center">
-        Your token balance: {balance / 1000000} {}
-      </h1>
-      <h1 className="text-lg font-bold text-center">Your merkle proofs:</h1>
-      <JsonPreview title="Merkle Proofs" content={proofs} />
+      {!wallet.initialized && (
+        <div className="flex justify-center items-center p-8 space-x-4 text-xl text-center text-white/50">
+          <CgSpinnerAlt className="animate-spin" />
+          <span>Loading...</span>
+        </div>
+      )}
 
-      <button
-        className={`btn bg-juno border-0 btn-lg hover:bg-juno/80 font-semibold text-2xl w-full mt-4 ${
-          loading ? 'loading' : ''
-        }`}
-        style={{ cursor: loading ? 'not-allowed' : 'pointer' }}
-        disabled={loading}
-        onClick={claim}
-      >
-        Claim Drop
-      </button>
-      <br />
-    </div>
+      {wallet.initialized && (
+        <div className="flex flex-col space-y-4">
+          <div className="flex items-center space-x-2">
+            <h3 className="text-2xl font-bold">{name}</h3>
+            <div className="flex-grow" />
+            <img
+              src="/juno_logo.png"
+              alt="juno"
+              className="w-6 h-6 rounded-full"
+            />
+            <span className="font-bold">{amount} juno</span>
+          </div>
+          <StackedList>
+            <StackedList.Item name="Airdrop Name">{name}</StackedList.Item>
+            <StackedList.Item name="Claim Amount">
+              {amount} juno
+            </StackedList.Item>
+            <StackedList.Item name="Merkle Proofs">
+              <pre className="p-2 text-sm whitespace-pre bg-stone-800/80 rounded">
+                {JSON.stringify(proofs, null, 2)}
+              </pre>
+            </StackedList.Item>
+          </StackedList>
+        </div>
+      )}
+
+      {wallet.initialized && (
+        <div className="flex justify-end pb-6">
+          <button
+            className={clsx(
+              'flex items-center py-2 px-8 space-x-2 font-bold bg-plumbus-50 hover:bg-plumbus-40 rounded',
+              'transition hover:translate-y-[-2px]',
+              { 'animate-pulse cursor-wait pointer-events-none': loading }
+            )}
+            disabled={loading}
+            onClick={claim}
+          >
+            {loading ? (
+              <CgSpinnerAlt className="animate-spin" />
+            ) : (
+              <FaAsterisk />
+            )}
+            <span>Claim Airdrop</span>
+          </button>
+        </div>
+      )}
+    </section>
   )
 }
 
-export async function getServerSideProps({
-  params,
-}: {
-  params: Record<string, string>
-}) {
-  return { props: { address: params.address } }
-}
-
-export default ClaimDrop
+export default withMetadata(ClaimAirdropPage, { center: false })

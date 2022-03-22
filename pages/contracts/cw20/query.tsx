f829b7f362e0a8f106c74378bc79027fb6a51850
Griko Nibras <git@griko.id>
2022-03-23 02:55:10 +0700

    feat(pages): create initial cw20 pages

---
+++ pages/contracts/cw20/query.tsx
@@ -0,0 +1,32 @@
+import Anchor from 'components/Anchor'
+import LinkTabs from 'components/LinkTabs'
+import { cw20LinkTabs } from 'components/LinkTabs.data'
+import { NextPage } from 'next'
+import { NextSeo } from 'next-seo'
+import { withMetadata } from 'utils/layout'
+import { links } from 'utils/links'
+
+const CW20QueryPage: NextPage = () => {
+  return (
+    <section className="py-6 px-12 space-y-4">
+      <NextSeo title="CW20 Tokens" />
+
+      <h1 className="font-heading text-4xl font-bold">CW20 Tokens</h1>
+      <p>
+        CW20 is a specification for fungible tokens based on CosmWasm. Learn
+        more in the{' '}
+        <Anchor
+          href={links['Docs CW20']}
+          className="font-bold text-plumbus hover:underline"
+        >
+          documentation page
+        </Anchor>
+        .
+      </p>
+
+      <LinkTabs data={cw20LinkTabs} activeIndex={1} />
+    </section>
+  )
+}
+
+export default withMetadata(CW20QueryPage, { center: false })

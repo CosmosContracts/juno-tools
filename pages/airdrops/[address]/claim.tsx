506299b465a7307d3493fe44bc569a14c7d9c5c8
findolor <anakisci@gmail.com>
2022-03-24 18:54:30 +0300

    Add additional logic and refactor

--- pages/airdrops/[address]/claim.tsx
+++ pages/airdrops/[address]/claim.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import clsx from 'clsx'
 import Alert from 'components/Alert'
+import Conditional from 'components/Conditional'
 import StackedList from 'components/StackedList'
 import { useContracts } from 'contexts/contracts'
 import { useWallet } from 'contexts/wallet'
@@ -13,12 +14,7 @@ import { CgSpinnerAlt } from 'react-icons/cg'
 import { FaAsterisk } from 'react-icons/fa'
 import { withMetadata } from 'utils/layout'
 
-type ClaimState =
-  | 'loading'
-  | 'not_claimed'
-  | 'claimed'
-  | 'no_allocation'
-  | 'not_allowed'
+type ClaimState = 'loading' | 'not_claimed' | 'claimed' | 'no_allocation'
 
 const ClaimAirdropPage: NextPage = () => {
   const router = useRouter()
@@ -38,31 +34,44 @@ const ClaimAirdropPage: NextPage = () => {
   const contractAddress = String(router.query.address)
 
   useEffect(() => {
-    if (!wallet.initialized) return
+    const getAirdropInfo = async () => {
+      try {
+        if (!wallet.initialized || contractAddress === '') return
 
-    axios
-      .get(
-        `${process.env.NEXT_PUBLIC_API_URL}/proofs/contract/${contractAddress}/wallet/${wallet.address}`
-      )
-      .then(({ data }) => {
+        const merkleAirdropContractMessages =
+          cw20MerkleAirdropContract?.use(contractAddress)
+
+        const { data } = await axios.get(
+          `${process.env.NEXT_PUBLIC_API_URL}/proofs/contract/${contractAddress}/wallet/${wallet.address}`
+        )
+
         const { account, airdrop } = data
         if (account) {
+          const stage = await merkleAirdropContractMessages?.getLatestStage()
+          const isClaimed = await merkleAirdropContractMessages?.isClaimed(
+            wallet.address,
+            stage || 0
+          )
+
           setProofs(account.proofs)
           setAmount(account.amount.toString())
           setName(airdrop.name)
           setCW20TokenAddress(airdrop.cw20TokenAddress)
-          setAirdropState('not_claimed')
+
+          if (isClaimed) setAirdropState('claimed')
+          else setAirdropState('not_claimed')
         } else {
-          toast.error("You don't have any tokens to claim!")
+          setAirdropState('no_allocation')
         }
-      })
-      .catch((err: any) => {
+      } catch (err: any) {
         setLoading(false)
         toast.error(err.message, {
           style: { maxWidth: 'none' },
         })
-        setAirdropState('not_allowed')
-      })
+      }
+    }
+
+    getAirdropInfo()
   }, [contractAddress, wallet.address, wallet.initialized])
 
   useEffect(() => {
@@ -93,7 +102,8 @@ const ClaimAirdropPage: NextPage = () => {
       await contractMessages?.claim(wallet.address, stage || 0, amount, proofs)
 
       setLoading(false)
-      toast.success('Success!', {
+      setAirdropState('claimed')
+      toast.success('Successfully claimed the airdrop!', {
         style: { maxWidth: 'none' },
       })
       setBalance(balance + parseInt(amount))
@@ -111,51 +121,28 @@ const ClaimAirdropPage: NextPage = () => {
 
       <div className="space-y-4">
         <h1 className="font-heading text-4xl font-bold">Claim Airdrop</h1>
-        {airdropState == 'loading' && (
+        <Conditional test={!wallet.initialized}>
           <Alert type="warning">
             No wallet detected. Please connect your wallet before claiming an
             airdrop.
           </Alert>
-        )}
-        {airdropState == 'no_allocation' && (
-          <Alert type="warning">
-            <b>No allocation.</b>
-            You do not have any claimable tokens for this airdrop address.
-          </Alert>
-        )}
-
-        {airdropState == 'not_claimed' && (
-          <p>
-            Click the claim airdrop button below to add{' '}
-            <b>{amount ?? '...'} juno</b> to your total balance.
-          </p>
-        )}
-
-        {airdropState == 'claimed' && (
-          <p>
-            This airdrop was claimed on{' '}
-            {
-              // TODO!: implement actual claimed date
-            }
-            {new Date().toLocaleString()}.
-          </p>
-        )}
-
-        {airdropState != 'loading' && airdropState != 'no_allocation' && (
-          <p>
-            Your current total token balance is <b>{balance / 1000000} juno</b>.
-          </p>
-        )}
+        </Conditional>
+        <Conditional test={wallet.initialized}>
+          {airdropState == 'no_allocation' && (
+            <Alert type="warning">
+              <b>No allocation.</b>
+              You do not have any claimable tokens for this airdrop address.
+            </Alert>
+          )}
+        </Conditional>
       </div>
 
-      {airdropState == 'loading' && (
-        <div className="flex justify-center items-center p-8 space-x-4 text-xl text-center text-white/50">
-          <CgSpinnerAlt className="animate-spin" />
-          <span>Loading...</span>
-        </div>
-      )}
-
-      {(airdropState == 'not_claimed' || airdropState == 'claimed') && (
+      <Conditional
+        test={
+          wallet.initialized &&
+          (airdropState === 'not_claimed' || airdropState == 'claimed')
+        }
+      >
         <div className="flex flex-col space-y-4">
           <div className="flex items-center space-x-2">
             <h3 className="text-2xl font-bold">{name}</h3>
@@ -165,12 +152,21 @@ const ClaimAirdropPage: NextPage = () => {
               alt="juno"
               className="w-6 h-6 rounded-full"
             />
-            <span className="font-bold">{amount} juno</span>
+            <span className="font-bold">{parseInt(amount) / 1000000} juno</span>
           </div>
           <StackedList>
             <StackedList.Item name="Airdrop Name">{name}</StackedList.Item>
+            <StackedList.Item name="Airdrop Contract Address">
+              {contractAddress}
+            </StackedList.Item>
+            <StackedList.Item name="Airdrop CW20 Token Address">
+              {cw20TokenAddress}
+            </StackedList.Item>
             <StackedList.Item name="Claim Amount">
-              {amount} juno
+              {parseInt(amount) / 1000000} juno
+            </StackedList.Item>
+            <StackedList.Item name="Your CW20 Balance">
+              {balance / 1000000} juno
             </StackedList.Item>
             <StackedList.Item name="Merkle Proofs">
               <pre className="overflow-auto p-2 text-sm bg-stone-800/80 rounded">
@@ -179,9 +175,9 @@ const ClaimAirdropPage: NextPage = () => {
             </StackedList.Item>
           </StackedList>
         </div>
-      )}
+      </Conditional>
 
-      {airdropState != 'loading' && airdropState != 'no_allocation' && (
+      <Conditional test={wallet.initialized}>
         <div className="flex justify-end pb-6">
           <button
             className={clsx(
@@ -194,7 +190,7 @@ const ClaimAirdropPage: NextPage = () => {
               }
             )}
             disabled={loading || airdropState != 'not_claimed'}
-            onClick={() => (airdropState != 'not_claimed' ? claim : null)}
+            onClick={claim}
           >
             {loading ? (
               <CgSpinnerAlt className="animate-spin" />
@@ -202,11 +198,11 @@ const ClaimAirdropPage: NextPage = () => {
               <FaAsterisk />
             )}
             <span>
-              {airdropState == 'claimed' ? 'Airdrop claimed' : 'Claim Airdrop'}
+              {airdropState == 'claimed' ? 'Airdrop Claimed' : 'Claim Airdrop'}
             </span>
           </button>
         </div>
-      )}
+      </Conditional>
     </section>
   )
 }

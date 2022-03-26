add9d79def1b7cce4e011e516b7fdbbda1be041e
Griko Nibras <git@griko.id>
2022-03-27 00:03:54 +0700

    feat(ui): update form control component

--- components/FormControl.tsx
+++ components/FormControl.tsx
@@ -5,7 +5,7 @@ type BaseProps<T = HTMLDivElement> = DetailedHTMLProps<HTMLAttributes<T>, T>
 
 export interface FormControlProps extends BaseProps {
   title: string
-  subtitle: ReactNode
+  subtitle?: ReactNode
   htmlId?: string
 }
 
@@ -16,7 +16,7 @@ const FormControl = (props: FormControlProps) => {
     <div className={clsx('flex flex-col space-y-2', className)} {...rest}>
       <label className="flex flex-col space-y-1" htmlFor={htmlId}>
         <span className="font-bold">{title}</span>
-        <span className="text-sm text-white/50">{subtitle}</span>
+        {subtitle && <span className="text-sm text-white/50">{subtitle}</span>}
       </label>
       {children}
     </div>

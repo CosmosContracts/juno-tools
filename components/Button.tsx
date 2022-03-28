0e5d26d8ecfa240a1e7970b61d7aa3b53cdee1e8
Griko Nibras <git@griko.id>
2022-03-29 04:48:22 +0700

    feat(ui): create button element

---
+++ components/Button.tsx
@@ -0,0 +1,55 @@
+import clsx from 'clsx'
+import { ButtonHTMLAttributes, DetailedHTMLProps } from 'react'
+import { CgSpinnerAlt } from 'react-icons/cg'
+
+export type ButtonVariant = 'solid' | 'outline'
+
+type BaseProps<T = HTMLButtonElement> = DetailedHTMLProps<
+  ButtonHTMLAttributes<T>,
+  T
+>
+
+export interface ButtonProps extends BaseProps {
+  isDisabled?: boolean
+  isLoading?: boolean
+  leftIcon?: JSX.Element
+  rightIcon?: JSX.Element
+  variant?: ButtonVariant
+}
+
+const Button = (props: ButtonProps) => {
+  const {
+    isDisabled,
+    isLoading,
+    leftIcon,
+    rightIcon,
+    variant = 'solid',
+    className,
+    children,
+    ...rest
+  } = props
+
+  return (
+    <button
+      className={clsx(
+        'group flex items-center py-2 px-4 space-x-2 font-bold focus:ring',
+        {
+          'bg-plumbus-60 hover:bg-plumbus-50 rounded ': variant == 'solid',
+          'bg-plumbus/10 hover:bg-plumbus/20 rounded border border-plumbus':
+            variant == 'outline',
+          'opacity-50 cursor-not-allowed pointer-events-none': isDisabled,
+          'animate-pulse cursor-wait pointer-events-none': isLoading,
+        },
+        className
+      )}
+      disabled={isDisabled}
+      {...rest}
+    >
+      {isLoading ? <CgSpinnerAlt className="animate-spin" /> : leftIcon}
+      <div>{children}</div>
+      {rightIcon}
+    </button>
+  )
+}
+
+export default Button

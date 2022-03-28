2ee3fb78f542e1e9f8204ce414dd2e05a0cb359a
Simon Arda Yuvarlak <simonyuvarlak@icloud.com>
2022-03-28 12:57:14 +0300

    page toggle

--- pages/contracts/cw3/components/InstantiateTimelock.tsx
+++ pages/contracts/cw3/components/InstantiateTimelock.tsx
@@ -5,7 +5,7 @@ const InstantiateTimelock = (props: {
     arg0: number,
     arg1: Record<string, unknown>,
     arg2: string,
-    arg3?: string | undefined
+    arg3?: string | undefined,
   ) => void
 }) => {
   const [codeId, setCodeId] = useState(626)
@@ -33,23 +33,31 @@ const InstantiateTimelock = (props: {
     }
   }
   return (
-    
-      <div>
-       
-        <div className=" relative ">
-          
-           <input type="text" id="contract-id" className=" flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent mt-3" placeholder="Admins"/>
-           <input type="text" id="contract-id" className=" flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent mt-3" placeholder="Proposers"/>
-           <input type="text" id="contract-id" className=" flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent mt-3" placeholder="Executors"/>
-           <input type="text" id="contract-id" className=" flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent mt-3" placeholder="Minimum Delay in nanosecs"/>
 
-          
-           <button className="p-3 bg-juno rounded-lg mt-3">
-              Instantiate
-           </button>
-        </div>
-       
-      
+    <div>
+
+      <div className=' relative '>
+
+        <input type='text' id='contract-id'
+               className=' flex-1 rounded appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent mt-3'
+               placeholder='Admins' />
+        <input type='text' id='contract-id'
+               className=' flex-1 rounded appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent mt-3'
+               placeholder='Proposers' />
+        <input type='text' id='contract-id'
+               className=' flex-1 rounded appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent mt-3'
+               placeholder='Executors' />
+        <input type='text' id='contract-id'
+               className=' flex-1 rounded appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent mt-3'
+               placeholder='Minimum Delay in nanosecs' />
+
+
+        <button className='p-3 bg-juno rounded-lg mt-3'>
+          Instantiate
+        </button>
+      </div>
+
+
       <br />
       {flag && <div></div>}
     </div>

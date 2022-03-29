95cb1e2facedcfd9edc1b42a4ffbf8b54bd3fecf
Griko Nibras <git@griko.id>
2022-03-30 02:47:50 +0700

    feat(ui): add form control required decor

--- components/FormControl.tsx
+++ components/FormControl.tsx
@@ -7,15 +7,23 @@ export interface FormControlProps extends BaseProps {
   title: string
   subtitle?: ReactNode
   htmlId?: string
+  isRequired?: boolean
 }
 
 const FormControl = (props: FormControlProps) => {
-  const { title, subtitle, htmlId, children, className, ...rest } = props
+  const { title, subtitle, htmlId, isRequired, children, className, ...rest } =
+    props
 
   return (
     <div className={clsx('flex flex-col space-y-2', className)} {...rest}>
       <label className="flex flex-col space-y-1" htmlFor={htmlId}>
-        <span className="font-bold">{title}</span>
+        <span
+          className={clsx('font-bold', {
+            "after:text-red-500 after:content-['_*']": isRequired,
+          })}
+        >
+          {title}
+        </span>
         {subtitle && <span className="text-sm text-white/50">{subtitle}</span>}
       </label>
       {children}

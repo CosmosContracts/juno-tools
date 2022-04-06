f42f006651768a74f6d083af093e18648223db63
Griko Nibras <git@griko.id>
2022-04-07 05:51:17 +0700

    refactor(ui): rework wallet button

--- components/WalletLoader.tsx
+++ components/WalletLoader.tsx
@@ -1,41 +1,99 @@
+import { Popover, Transition } from '@headlessui/react'
 import clsx from 'clsx'
-import { useWallet } from 'contexts/wallet'
-import { BiWallet } from 'react-icons/bi'
-import { FaSpinner } from 'react-icons/fa'
+import { useWallet, useWalletStore } from 'contexts/wallet'
+import { Fragment, VFC } from 'react'
+import { FaCopy, FaPowerOff, FaRedo } from 'react-icons/fa'
+import { copy } from 'utils/clipboard'
 import getShortAddress from 'utils/getShortAddress'
 
-const WalletLoader = () => {
+import { WalletButton } from './WalletButton'
+import { WalletPanelButton } from './WalletPanelButton'
+
+export const WalletLoader: VFC = () => {
   const wallet = useWallet()
 
-  const handleClick = () => {
-    return (wallet.initialized ? wallet.disconnect : wallet.connect)(true)
-  }
+  const connect = useWalletStore((store) => store.connect)
+  const disconnect = useWalletStore((store) => store.disconnect)
+  const isLoading = useWalletStore((store) => store.initializing)
+  const isReady = useWalletStore((store) => store.initialized)
+
+  const displayName = useWalletStore(
+    (store) => store.name || getShortAddress(store.address)
+  )
 
   return (
-    <button
-      className={clsx(
-        'flex gap-x-2 items-center text-sm font-bold uppercase truncate', // content styling
-        'py-2 px-4 my-8 border border-plumbus', // button styling
-        'hover:bg-white/10 transition-colors', // hover styling
-        { 'cursor-wait': wallet.initializing } // loading styling
-      )}
-      disabled={wallet.initializing}
-      onClick={handleClick}
-    >
-      {wallet.initializing ? (
-        <FaSpinner size={16} className="animate-spin" />
-      ) : (
-        <BiWallet size={16} />
-      )}
+    <Popover className="my-8">
+      {({ close }) => (
+        <Fragment>
+          <div className="grid -mx-4">
+            {!isReady && (
+              <WalletButton
+                className="w-full"
+                isLoading={isLoading}
+                onClick={() => connect()}
+              >
+                Connect Wallet
+              </WalletButton>
+            )}
 
-      <span>
-        {wallet.initializing
-          ? 'Loading...'
-          : wallet.initialized
-          ? wallet.name || getShortAddress(wallet.address)
-          : 'Connect Wallet'}
-      </span>
-    </button>
+            {isReady && (
+              <Popover.Button
+                className="w-full"
+                as={WalletButton}
+                isLoading={isLoading}
+              >
+                {displayName}
+              </Popover.Button>
+            )}
+          </div>
+
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-200"
+            enterFrom="opacity-0 -translate-y-1"
+            enterTo="opacity-100 translate-y-0"
+            leave="transition ease-in duration-150"
+            leaveFrom="opacity-100 translate-y-0"
+            leaveTo="opacity-0 -translate-y-1"
+          >
+            <Popover.Panel
+              className={clsx(
+                'absolute inset-x-4 mt-2',
+                'bg-stone-800/80 rounded shadow-lg shadow-black/90 backdrop-blur-sm',
+                'flex flex-col items-stretch text-sm divide-y divide-white/10'
+              )}
+            >
+              <div className="flex flex-col items-center py-2 px-4 space-y-1 text-center">
+                <span className="py-px px-2 mb-2 font-mono text-xs text-white/50 rounded-full border border-white/25">
+                  {getShortAddress(wallet.address)}
+                </span>
+                <div className="font-bold">Your Balances</div>
+                {wallet.balance.map((val) => (
+                  <span key={`balance-${val.denom}`}>
+                    {val.amount} {val.denom}
+                  </span>
+                ))}
+              </div>
+              <WalletPanelButton
+                Icon={FaCopy}
+                onClick={() => copy(wallet.address)}
+              >
+                Copy wallet address
+              </WalletPanelButton>
+              <WalletPanelButton Icon={FaRedo} onClick={() => connect()}>
+                Reconnect
+              </WalletPanelButton>
+              <WalletPanelButton
+                Icon={FaPowerOff}
+                onClick={() => (disconnect(), close())}
+              >
+                Disconnect
+              </WalletPanelButton>
+            </Popover.Panel>
+          </Transition>
+        </Fragment>
+      )}
+    </Popover>
   )
 }
 

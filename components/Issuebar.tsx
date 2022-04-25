15bfcbea5082a6a01d3ba1c367b5426c421a1274
findolor <anakisci@gmail.com>
2022-04-25 19:16:27 +0300

    Change issuebar size

--- components/Issuebar.tsx
+++ components/Issuebar.tsx
@@ -9,11 +9,11 @@ const Issuebar = () => {
   return (
     <div
       className={clsx(
-        'overflow-auto min-w-[250px] max-w-[250px] no-scrollbar',
+        'overflow-auto min-w-[230px] max-w-[230px] no-scrollbar',
         'bg-black/50 border-l-[1px] border-l-plumbus-light'
       )}
     >
-      <div className="flex flex-col gap-y-4 p-8 min-h-screen">
+      <div className="flex flex-col gap-y-4 p-6 pt-8 min-h-screen">
         <div>This is a beta version of JunoTools.</div>
         <div>We are open for your feedback and suggestions!</div>
         <Anchor href={links.GitHub + '/issues/new'}>

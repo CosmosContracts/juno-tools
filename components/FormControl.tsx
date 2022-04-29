4872ba81f2b8414e1e1b349fb9af79a5bce0f685
Griko Nibras <git@griko.id>
2022-04-30 03:58:25 +0700

    feat(ui): polymorphic form control label

--- components/FormControl.tsx
+++ components/FormControl.tsx
@@ -1,19 +1,20 @@
 import clsx from 'clsx'
-import type { ComponentProps, ReactNode } from 'react'
+import type { ComponentProps, ElementType, ReactNode } from 'react'
 
 export interface FormControlProps extends ComponentProps<'div'> {
   title: string
   subtitle?: ReactNode
   htmlId?: string
   isRequired?: boolean
+  labelAs?: ElementType
 }
 
 export const FormControl = (props: FormControlProps) => {
-  const { title, subtitle, htmlId, isRequired, children, className, ...rest } = props
+  const { title, subtitle, htmlId, isRequired, children, className, labelAs: Label = 'label', ...rest } = props
 
   return (
     <div className={clsx('flex flex-col space-y-2', className)} {...rest}>
-      <label className="flex flex-col space-y-1" htmlFor={htmlId}>
+      <Label className="flex flex-col space-y-1" htmlFor={htmlId}>
         <span
           className={clsx('font-bold', {
             "after:text-red-500 after:content-['_*']": isRequired,
@@ -22,7 +23,7 @@ export const FormControl = (props: FormControlProps) => {
           {title}
         </span>
         {subtitle && <span className="text-sm text-white/50">{subtitle}</span>}
-      </label>
+      </Label>
       {children}
     </div>
   )

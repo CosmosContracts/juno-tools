355f749d62e7ef9f757307e462c1f088c2fd5557
Griko Nibras <git@griko.id>
2022-04-30 05:32:31 +0700

    feat(pages): wip cw20 execute page

--- pages/contracts/cw20/execute.tsx
+++ pages/contracts/cw20/execute.tsx
@@ -1,3 +1,7 @@
+import { ExecuteCombobox } from 'components/cw20/ExecuteCombobox'
+import { useExecuteComboboxState } from 'components/cw20/ExecuteCombobox.hooks'
+import { AddressInput } from 'components/forms/AddressInput'
+import { useAddressInputState } from 'components/forms/AddressInput.hooks'
 import { LinkTabs } from 'components/LinkTabs'
 import { cw20LinkTabs } from 'components/LinkTabs.data'
 import { PageHeaderCw20 } from 'components/PageHeaderCw20'
@@ -6,13 +10,26 @@ import { NextSeo } from 'next-seo'
 import { withMetadata } from 'utils/layout'
 
 const CW20ExecutePage: NextPage = () => {
+  const addressState = useAddressInputState({
+    id: 'sender-address',
+    name: 'sender',
+    title: 'Sender Address',
+    subtitle: 'Address of the CW20 token',
+  })
+  const comboboxState = useExecuteComboboxState()
+
   return (
     <section className="py-6 px-12 space-y-4">
       <NextSeo title="Execute CW20 Token" />
-
       <PageHeaderCw20 />
-
       <LinkTabs activeIndex={2} data={cw20LinkTabs} />
+
+      <form className="grid grid-cols-2 p-4 space-x-8">
+        <div className="space-y-8">
+          <AddressInput {...addressState} />
+          <ExecuteCombobox {...comboboxState} />
+        </div>
+      </form>
     </section>
   )
 }

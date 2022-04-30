e9592916d782ca5459e4360297718234184fdeb2
Griko Nibras <git@griko.id>
2022-05-01 00:39:24 +0700

    chore(ui): deprecate Input to StyledInput

--- components/Input.tsx
+++ components/Input.tsx
@@ -2,17 +2,23 @@ import clsx from 'clsx'
 import type { ComponentProps } from 'react'
 import { forwardRef } from 'react'
 
-export const Input = forwardRef<HTMLInputElement, ComponentProps<'input'>>(function Input({ className, ...rest }, ref) {
-  return (
-    <input
-      className={clsx(
-        'bg-white/10 rounded border-2 border-white/20 form-input',
-        'placeholder:text-white/50',
-        'focus:ring focus:ring-plumbus-20',
-        className,
-      )}
-      ref={ref}
-      {...rest}
-    />
-  )
-})
+export const StyledInput = forwardRef<HTMLInputElement, ComponentProps<'input'>>(
+  function Input({ className, ...rest }, ref) {
+    return (
+      <input
+        className={clsx(
+          'bg-white/10 rounded border-2 border-white/20 form-input',
+          'placeholder:text-white/50',
+          'focus:ring focus:ring-plumbus-20',
+          className,
+        )}
+        ref={ref}
+        {...rest}
+      />
+    )
+  },
+  //
+)
+
+/** @deprecated - replace with {@link StyledInput} */
+export const Input = StyledInput

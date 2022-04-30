c49c80e1f24474b3c1e6ceecd8c94451dfd9f29e
Griko Nibras <git@griko.id>
2022-05-01 05:28:24 +0700

    feat(pages): wip cw20 execute page

--- pages/contracts/cw20/execute.tsx
+++ pages/contracts/cw20/execute.tsx
@@ -1,8 +1,12 @@
+import clsx from 'clsx'
 import { Button } from 'components/Button'
 import { ExecuteCombobox } from 'components/cw20/ExecuteCombobox'
 import { useExecuteComboboxState } from 'components/cw20/ExecuteCombobox.hooks'
+import { FormControl } from 'components/FormControl'
 import { AddressInput, NumberInput } from 'components/forms/FormInput'
 import { useInputState, useNumberInputState } from 'components/forms/FormInput.hooks'
+import { StyledInput } from 'components/Input'
+import { JsonPreview } from 'components/JsonPreview'
 import { LinkTabs } from 'components/LinkTabs'
 import { cw20LinkTabs } from 'components/LinkTabs.data'
 import { PageHeaderCw20 } from 'components/PageHeaderCw20'
@@ -13,8 +17,9 @@ import { NextSeo } from 'next-seo'
 import type { FormEvent } from 'react'
 import { useState } from 'react'
 import { toast } from 'react-hot-toast'
+import { FaArrowRight } from 'react-icons/fa'
 import { useMutation } from 'react-query'
-import { dispatchExecute, isEitherType } from 'utils/cw20/execute'
+import { dispatchExecute, dispatchPreviewPayload, isEitherType } from 'utils/cw20/execute'
 import { withMetadata } from 'utils/layout'
 
 const CW20ExecutePage: NextPage = () => {
@@ -122,9 +127,36 @@ const CW20ExecutePage: NextPage = () => {
           {showAmountField && <NumberInput {...amountState} />}
         </div>
         <div className="space-y-8">
-          <Button isLoading={isLoading} type="submit">
-            Submit
-          </Button>
+          <div className="relative">
+            <Button className="absolute top-0 right-0" isLoading={isLoading} rightIcon={<FaArrowRight />} type="submit">
+              Execute
+            </Button>
+            <FormControl
+              className={lastTx ? 'visible' : 'invisible'}
+              subtitle="View previous execution transaction hash"
+              title="Transaction Hash"
+            >
+              <StyledInput className={clsx(lastTx ? 'select-all' : 'select-none')} readOnly value={lastTx || '...'} />
+            </FormControl>
+          </div>
+          <FormControl subtitle="View current data to be sent" title="Payload Preview">
+            <JsonPreview
+              content={dispatchPreviewPayload({
+                amount: amountState.value.toString(),
+                contract: contractState.value,
+                description: '',
+                logo: { url: '' },
+                marketing: '',
+                msg: {},
+                owner: ownerState.value,
+                project: '',
+                recipient: recipientState.value,
+                txSigner,
+                type,
+              })}
+              title={type ? `${type} payload preview` : 'payload preview'}
+            />
+          </FormControl>
         </div>
       </form>
     </section>

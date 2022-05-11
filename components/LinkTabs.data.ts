22bc2362d85fcc298c7ee805d7b74b34e5454655
findolor <anakisci@gmail.com>
2022-05-11 14:35:39 +0300

    Add cw1 subkeys links to component

--- components/LinkTabs.data.ts
+++ components/LinkTabs.data.ts
@@ -17,3 +17,21 @@ export const cw20LinkTabs: LinkTabProps[] = [
     href: '/contracts/cw20/execute',
   },
 ]
+
+export const cw1SubkeysLinkTabs: LinkTabProps[] = [
+  {
+    title: 'Instantiate',
+    description: `Create a new CW1 Subkeys contract`,
+    href: '/contracts/cw1/subkeys/instantiate',
+  },
+  {
+    title: 'Query',
+    description: `Dispatch queries with your CW1 Subkeys contract`,
+    href: '/contracts/cw1/subkeys/query',
+  },
+  {
+    title: 'Execute',
+    description: `Execute CW1 Subkeys contract actions`,
+    href: '/contracts/cw1/subkeys/execute',
+  },
+]

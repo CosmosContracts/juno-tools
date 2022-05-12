74ba2d78d7011b876a4024c9bde5a1e9360662ed
findolor <anakisci@gmail.com>
2022-05-12 13:48:51 +0300

    Add cw721 base links

--- components/LinkTabs.data.ts
+++ components/LinkTabs.data.ts
@@ -35,3 +35,21 @@ export const cw1SubkeysLinkTabs: LinkTabProps[] = [
     href: '/contracts/cw1/subkeys/execute',
   },
 ]
+
+export const cw721BaseLinkTabs: LinkTabProps[] = [
+  {
+    title: 'Instantiate',
+    description: `Create a new CW721 Base contract`,
+    href: '/contracts/cw721/base/instantiate',
+  },
+  {
+    title: 'Query',
+    description: `Dispatch queries with your CW721 Base contract`,
+    href: '/contracts/cw721/base/query',
+  },
+  {
+    title: 'Execute',
+    description: `Execute CW721 Base contract actions`,
+    href: '/contracts/cw721/base/execute',
+  },
+]

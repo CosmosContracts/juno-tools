86a60d3a806e9fa3e6d38ca71e57326c4d2663c6
findolor <anakisci@gmail.com>
2022-05-16 08:26:11 +0300

    Delete extra files

--- components/cw20/ExecuteCombobox.hooks.ts
+++
@@ -1,7 +0,0 @@
-import { useState } from 'react'
-import type { ExecuteListItem } from 'utils/cw20/execute'
-
-export const useExecuteComboboxState = () => {
-  const [value, setValue] = useState<ExecuteListItem | null>(null)
-  return { value, onChange: (item: ExecuteListItem) => setValue(item) }
-}

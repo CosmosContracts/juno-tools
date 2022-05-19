a3405b965ea822a820eba7d06683175b416ceb40
findolor <anakisci@gmail.com>
2022-05-19 15:32:42 +0300

    Add custom contract link to sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -11,6 +11,7 @@ import { WalletLoader } from './WalletLoader'
 const routes = [
   { text: 'Airdrops', href: `/airdrops` },
   { text: 'Upload Contract', href: `/contracts/upload` },
+  { text: 'Custom Contract', href: `/contracts/custom` },
   { text: 'CW1 Subkeys', href: `/contracts/cw1/subkeys` },
   { text: 'CW20 Base', href: `/contracts/cw20/base` },
   { text: 'CW721 Base', href: `/contracts/cw721/base` },

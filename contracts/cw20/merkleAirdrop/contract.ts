8e569e8d0afd72207d56c7b0b4f4347e0bbf4c96
findolor <anakisci@gmail.com>
2022-07-04 16:05:16 +0300

    Revert "Merge branch 'testnet' into development"
    This reverts commit f370b816415cc988df5e2ed8eb033204dc9d7834, reversing
    changes made to c94efb393cec1f6da4d855e796293ab884d752df.

--- contracts/cw20/merkleAirdrop/contract.ts
+++ contracts/cw20/merkleAirdrop/contract.ts
@@ -97,6 +97,13 @@ export interface CW20MerkleAirdropMessages {
     hrp?: string,
   ) => [RegisterMessage, ReleaseEscrowMessage]
   depositEscrow: (airdropAddress: string) => DepositEscrowMessage
+  claim: (
+    airdropAddress: string,
+    stage: number,
+    amount: string,
+    proof: string[],
+    signedMessage?: SignedMessage,
+  ) => ClaimMessage
   fundWithSend: (recipient: string, amount: string) => FundWithSendMessage
   burn: (airdropAddress: string, stage: number) => BurnMessage
   withdraw: (airdropAddress: string, stage: number, address: string) => WithdrawMessage
@@ -190,12 +197,6 @@ export interface WithdrawMessage {
     }
   }
   funds: Coin[]
-}
-
-export interface FundWithSendMessage {
-  from_address: string
-  to_address: string
-  amount: Coin[]
 }
 
 export interface CW20MerkleAirdropContract {
@@ -596,14 +597,6 @@ export const CW20MerkleAirdrop = (client: SigningCosmWasmClient, txSigner: strin
           },
         },
         funds: [],
-      }
-    }
-
-    const fundWithSend = (recipient: string, amount: string): FundWithSendMessage => {
-      return {
-        from_address: txSigner,
-        to_address: recipient,
-        amount: [coin(amount, getNetworkConfig(NETWORK).feeToken)],
       }
     }
 

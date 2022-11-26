8e9caa9c03aa022063d10c2879c6460c7dcf40c8
Serkan Reis <serkanreis@gmail.com>
2022-11-26 17:45:39 +0300

    Remove Chakra UI Provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,33 +3,27 @@ import '@fontsource/roboto/latin.css'
 import '../styles/globals.css'
 import '../styles/datepicker.css'
 
-import { ChakraProvider } from '@chakra-ui/react'
 import type { SignerOptions } from '@cosmos-kit/core'
 import { wallets as keplrWallets } from '@cosmos-kit/keplr'
 import { WalletProvider } from '@cosmos-kit/react'
-import type { WalletControllerChainOptions } from '@terra-money/wallet-provider'
 import { assets, chains } from 'chain-registry'
 import { Layout } from 'components/Layout'
 import { Modal } from 'components/Modal'
 import type { AppProps } from 'next/app'
 import { getComponentMetadata } from 'utils/layout'
 
-import { defaultTheme } from '../config/theme'
-
-export default function App({ Component, pageProps }: AppProps & WalletControllerChainOptions) {
+export default function App({ Component, pageProps }: AppProps) {
   const signerOptions: SignerOptions = {
     // stargate: (_chain: Chain) => {
     //   return getSigningCosmosClientOptions();
     // }
   }
   return (
-    <ChakraProvider resetCSS={false} theme={defaultTheme}>
-      <WalletProvider assetLists={assets} chains={chains} signerOptions={signerOptions} wallets={[...keplrWallets]}>
-        <Layout metadata={getComponentMetadata(Component)}>
-          <Component {...pageProps} />
-          <Modal />
-        </Layout>
-      </WalletProvider>
-    </ChakraProvider>
+    <WalletProvider assetLists={assets} chains={chains} signerOptions={signerOptions} wallets={[...keplrWallets]}>
+      <Layout metadata={getComponentMetadata(Component)}>
+        <Component {...pageProps} />
+        <Modal />
+      </Layout>
+    </WalletProvider>
   )
 }

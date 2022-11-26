e7fa962de25b0415d5e88d1c13454a668ebbb992
Serkan Reis <serkanreis@gmail.com>
2022-11-26 18:14:10 +0300

    app & index.tsx overhaul

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,10 +1,8 @@
 import clsx from 'clsx'
-import Head from 'next/head'
 import type { ReactNode } from 'react'
 import { FaDesktop } from 'react-icons/fa'
 import type { PageMetadata } from 'utils/layout'
 
-import { DefaultAppSeo } from './DefaultAppSeo'
 // import { Issuebar } from './Issuebar'
 
 export interface LayoutProps {
@@ -15,12 +13,6 @@ export interface LayoutProps {
 export const Layout = ({ children, metadata = {} }: LayoutProps) => {
   return (
     <div className="overflow-hidden relative">
-      <Head>
-        <meta content="minimum-scale=1, initial-scale=1, width=device-width" name="viewport" />
-      </Head>
-
-      <DefaultAppSeo />
-
       {/* plumbus confetti */}
       <div className="fixed inset-0 -z-10 pointer-events-none juno-gradient-bg">
         <img alt="plumbus confetti" className="fixed top-0 right-0 h-[calc(100vh+180px)]" src="/confetti.svg" />
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,28 @@
-import '@fontsource/jetbrains-mono/latin.css'
-import '@fontsource/roboto/latin.css'
 import '../styles/globals.css'
-import '../styles/datepicker.css'
 
+import { ChakraProvider } from '@chakra-ui/react'
 import type { SignerOptions } from '@cosmos-kit/core'
 import { wallets as keplrWallets } from '@cosmos-kit/keplr'
 import { WalletProvider } from '@cosmos-kit/react'
 import { assets, chains } from 'chain-registry'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
+import { defaultTheme } from '../config/theme'
+
+function CreateCosmosApp({ Component, pageProps }: AppProps) {
   const signerOptions: SignerOptions = {
     // stargate: (_chain: Chain) => {
     //   return getSigningCosmosClientOptions();
     // }
   }
+
   return (
-    <WalletProvider assetLists={assets} chains={chains} signerOptions={signerOptions} wallets={[...keplrWallets]}>
-      <Component {...pageProps} />
-    </WalletProvider>
+    <ChakraProvider theme={defaultTheme}>
+      <WalletProvider assetLists={assets} chains={chains} signerOptions={signerOptions} wallets={[...keplrWallets]}>
+        <Component {...pageProps} />
+      </WalletProvider>
+    </ChakraProvider>
   )
 }
+
+export default CreateCosmosApp

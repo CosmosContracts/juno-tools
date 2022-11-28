67c592cf6613dcc08e9317b1bc12585001d8dfa0
Serkan Reis <serkanreis@gmail.com>
2022-11-28 16:32:26 +0300

    v0.13.0 vanilla

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,6 @@ import { WalletProvider } from '@cosmos-kit/react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { defaultTheme, chainName } from '../config';
 import { wallets as keplrWallets } from '@cosmos-kit/keplr';
-import { wallets as cosmostationWallets } from '@cosmos-kit/cosmostation';
-import { wallets as leapWallets } from '@cosmos-kit/leap';
 
 
 import { SignerOptions } from '@cosmos-kit/core';
@@ -23,7 +21,7 @@ function CreateCosmosApp({ Component, pageProps }: AppProps) {
       <WalletProvider
         chains={chains}
         assetLists={assets}
-        wallets={[...keplrWallets, ...cosmostationWallets, ...leapWallets]}
+        wallets={[...keplrWallets]}
         signerOptions={signerOptions}
       >
         <Component {...pageProps} />

5969d12165e02ec30e03a16a33e2d5a266714498
Serkan Reis <serkanreis@gmail.com>
2022-11-28 09:30:22 +0300

    Add Cosmostation & Leap Wallets

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,9 @@ import '../styles/globals.css'
 
 import { ChakraProvider } from '@chakra-ui/react'
 import type { SignerOptions } from '@cosmos-kit/core'
+import { wallets as cosmostationWallets } from '@cosmos-kit/cosmostation'
 import { wallets as keplrWallets } from '@cosmos-kit/keplr'
+import { wallets as leapWallets } from '@cosmos-kit/leap'
 import { WalletProvider } from '@cosmos-kit/react'
 import { assets, chains } from 'chain-registry'
 import type { AppProps } from 'next/app'
@@ -18,7 +20,12 @@ function CreateCosmosApp({ Component, pageProps }: AppProps) {
 
   return (
     <ChakraProvider theme={defaultTheme}>
-      <WalletProvider assetLists={assets} chains={chains} signerOptions={signerOptions} wallets={[...keplrWallets]}>
+      <WalletProvider
+        assetLists={assets}
+        chains={chains}
+        signerOptions={signerOptions}
+        wallets={[...keplrWallets, ...cosmostationWallets, ...leapWallets]}
+      >
         <Component {...pageProps} />
       </WalletProvider>
     </ChakraProvider>
